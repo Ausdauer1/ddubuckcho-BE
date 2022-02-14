0ecfd33a2355fd989baf9b9f9b63b8a2d0060313
Ausdauer1 <97427803+Ausdauer1@users.noreply.github.com>
2022-02-14 20:15:03 +0900

    ip주소로

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
   mongoose
-    .connect(process.env.MONGO_URL || 'mongodb://localhost:27017/ddubuckcho', {
+    .connect(process.env.MONGO_URL || 'mongodb://3.36.122.203:27017/ddubuckcho', {
       ignoreUndefined: true,
     })
     .catch((error) => {
